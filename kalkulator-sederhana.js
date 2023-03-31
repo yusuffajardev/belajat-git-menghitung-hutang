38b3c35986f2ac0aba7f7f780ca9e18172518695
yusuffajardev <yusuffajardev@outlook.com>
2023-03-31 17:50:43 +0700

    Membuat file kalkulator-sederhana.js, dan membuat fitur pertambahan

---
+++ kalkulator-sederhana.js
@@ -0,0 +1,9 @@
+// program kalkulator sederhana
+// deklarasi
+// bilangan, hasil
+
+function tambah(bil1, bil2) {
+  let hasil = bil1 + bil2;
+  return hasil;
+}
+console.log(`Hasil pertambahan ${tambah(2, 1)}`);
